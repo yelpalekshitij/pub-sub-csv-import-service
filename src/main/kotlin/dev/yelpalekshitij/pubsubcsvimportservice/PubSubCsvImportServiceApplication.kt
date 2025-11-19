b66845cf4319a53495c2232016148aa85d94962a
Kshitij Yelpale <kshitij.yelpale@gmail.com>
2025-11-20 00:58:56 +0100

    Initial commit

---
+++ src/main/kotlin/dev/yelpalekshitij/pubsubcsvimportservice/PubSubCsvImportServiceApplication.kt
@@ -0,0 +1,11 @@
+package dev.yelpalekshitij.pubsubcsvimportservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PubSubCsvImportServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<PubSubCsvImportServiceApplication>(*args)
+}
---
+++ src/test/kotlin/dev/yelpalekshitij/pubsubcsvimportservice/PubSubCsvImportServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.yelpalekshitij.pubsubcsvimportservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PubSubCsvImportServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
